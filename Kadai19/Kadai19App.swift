3572ba2a950283eb77b4566ab757821c2b7eb93b
mana.u <great8life@gmail.com>
2022-01-21 19:39:20 +0900

    Initial Commit

---
+++ Kadai19/Kadai19App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai19App.swift
+//  Kadai19
+//
+//  Created by mana on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Kadai19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
